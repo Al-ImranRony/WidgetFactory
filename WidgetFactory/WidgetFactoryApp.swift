954c3276ec262385f66563ca6be481665b65a1dc
ImranRony <imranrony687@gmail.com>
2023-03-23 15:53:45 +0600

    Initial Commit

---
+++ WidgetFactory/WidgetFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetFactoryApp.swift
+//  WidgetFactory
+//
+//  Created by Bitmorpher 4 on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
